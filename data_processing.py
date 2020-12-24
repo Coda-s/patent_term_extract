a40689d179674c18798933dbfc1ab2857a965b64
Coda-s <coda@DESKTOP-7RHL4TN.localdomain>
2020-12-24 13:03:06 +0800

    Coda

---
+++ data_processing.py
@@ -0,0 +1,70 @@
+import os
+import re
+
+def func(matched):
+    s = ""
+    for word in matched.group():
+        if word != '(' and word != '[' and word != ')' and word != ']':
+            s += 'I'
+    s = 'B' + s[1:]
+    return s
+
+def get_text_target(path):
+    """
+    generate the data for training and testing from source data
+    """
+    """ get_texts """
+    text = []
+    with open(os.path.join("data", path), "r", encoding="UTF-8") as f:
+        lines = [line.strip() for line in f.readlines()]
+        for line in lines:
+            line = re.sub(r"\[\[\[\(\(\(", "", line)
+            line = re.sub(r"\)\)\)\]\]\]", "", line)
+            text.append(line)
+    with open(os.path.join("processed_data", path, "text"), "w", encoding="UTF-8") as f:
+        for line in text:
+            f.write(line+'\n')
+    """ get_target """
+    label = []
+    pattern = re.compile(r'\[\[\[\(\(\([^\[^\]]*\)\)\)\]\]\]')
+    with open(os.path.join("data", path), "r", encoding="UTF-8") as f:
+        lines = [line.strip() for line in f.readlines()]
+        for line in lines:
+            line = re.sub(pattern, func, line)
+            line = re.sub(r"\[\[\[\(\(\(", "", line)
+            line = re.sub(r"\)\)\)\]\]\]", "", line)
+            line = re.sub(r"[^BI]", "O", line)
+            label.append(line)
+    with open(os.path.join("processed_data", path, "target"), "w", encoding="UTF-8") as f:
+        for line in label:
+            f.write(line+'\n')
+
+def check_data(path):
+    """
+    checking the data 
+    return True or False
+    """
+    with open(os.path.join("processed_data", path, "text")) as ftext:
+        with open(os.path.join("processed_data", path, "target")) as ftarget:
+            text_lines = [line.strip() for line in ftext.readlines()]
+            target_lines = [line.strip() for line in ftarget]
+            for text, target in zip(text_lines, target_lines):
+                if(len(text) != len(target)):
+                    return False
+                for i in target:
+                    if i!='B' and i!='I' and i!='O':
+                        return False
+            return True
+
+if __name__ == "__main__":
+    print("processing...")
+    get_text_target("train")
+    get_text_target("dev")
+    get_text_target("test")
+
+    print("checking...")
+    if check_data("train") is False or check_data("dev") is False or check_data("test") is False:
+        print("Error!!!")
+    else:
+        print("Ok!!!")
+    
